aad52bd0110a7d6e84698c64fe9e67d0c1ad2e1c
Tianyu Luo <tianyuluo@Tianyus-MacBook-Air.local>
2024-09-09 18:12:13 -0400

    Add method to calculate number of steps for 100 to reduce to 0

---
+++ src/Reduce.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+    int n = 100;
+    int step = 0;
+    while (n > 0) {
+        if (n % 2 == 0) {
+            n  /= 2;
+        } else{
+            n -= 1;
+        }
+        step++;
+    }
+    System.out.println(step);
+}
