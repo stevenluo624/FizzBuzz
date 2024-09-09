2cd195a76a5f20e0e30d856c628ef2fb4026eeba
Tianyu Luo <tianyuluo@Tianyus-MacBook-Air.local>
2024-09-09 18:07:45 -0400

    add method to find number of multiples of 3 and 5 below 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public static void main(String[] args) {
+    int num = 0;
+    for (int i = 1; i <= 1000; i++) {
+        boolean divisibleby3 = i % 3 == 0;
+        boolean divisibleby5 = i % 3 == 0;
 
-public class Multiples {
+        if (divisibleby3 || divisibleby5) {
+            num++;
+        }
+    }
+    System.out.println(num);
 }
